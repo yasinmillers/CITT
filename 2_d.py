f51fa8efabc3c575cae84dbd53abc1e117ff87a3
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-03-03 16:59:36 +0300

    printing

--- 2_d.py
+++ 2_d.py
@@ -4,3 +4,10 @@ import random
 
 d_array= np.array([[23,45,43,23,45],[45,67,54,32,45]])
 print(d_array[1,1])
+
+list=[29,45,67,87,85,85,87]
+
+def median1(n):
+    y=np.median(list)
+    return n + str(y)
+print(median1("median is" ))
