19c3eaf7a69fd828b7f7141ff716eb35f9cb415e
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-03-14 16:59:35 +0300

    visualise

---
+++ visualise.py
@@ -0,0 +1,13 @@
+from turtle import color
+
+
+def Artist():
+    import matplotlib.pyplot as plt
+    artist = ["rihahna", "sia", "ariana", "eminem", "lil pump", "lil wayne", "lil uzi vert",
+              "lil wayne", "lil uzi vert", "lil pump", "eminem", "ariana", "sia", "rihahna"]
+    sales = [19500, 2909, 36500, 44455, 5999, 645,
+             709, 890, 945, 1000, 16771, 1209, 13787]
+    color = ["red", "blue", "green", "yellow", "black", "pink", "orange",
+             "purple", "brown", "grey", "white", "cyan", "magenta"]  # color list
+
+    fig, ax = plt.subplots(nrows=2, ncols=2)
