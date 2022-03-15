18b384c34533d9241d60abd2aae12c52be2999c9
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-03-15 16:53:21 +0300

    adding data

--- loc.py
+++ loc.py
@@ -3,3 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 plt.style.use('dark_background')
+
+
+car_sales = {
+    "bmw": [2000, 4000, 66000, 78000, 100000],
+    "lambo": [15000, 30000, 50000, 70000, 100000],
+    "nissan": [10000, 20000, 40000, 60000, 80000],
+
+
+}
