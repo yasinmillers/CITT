5396aa7f212ac7c6214e28ef729014f0c35a4dd3
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-03-15 17:42:06 +0300

    new cases

--- readingcsv.py
+++ readingcsv.py
@@ -1,4 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-data = pd.read_csv('data.csv')
+
+font1 = {'family': 'serif',
+         'color':  'red',
+         'weight': 'normal',
+         'size': 20, }
+
+data = pd.read_csv('data.csv', skiprows=3)
+print(data)
+data.plot(kind='New Cases')
+plt.title('New Cases', fontdict=font1)
+plt.show()
