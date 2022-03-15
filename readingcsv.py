43fc098f73b533ef935d1a7d366ab809b10e6867
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-03-15 17:54:28 +0300

    graphs read

--- readingcsv.py
+++ readingcsv.py
@@ -8,7 +8,15 @@ font1 = {'family': 'serif',
          'size': 20, }
 
 data = pd.read_csv('data.csv', skiprows=3)
+# print(data)
+# print(data.info)
+# data.plot(kind='bar')
+# plt.title('New Cases', fontdict=font1)
+# plt.show()
+
 print(data)
-data.plot(kind='New Cases')
-plt.title('New Cases', fontdict=font1)
+print(data.info)
+
+data.plot(kind='bar')
+plt.title("Covid Data", fontdict=font1)
 plt.show()
